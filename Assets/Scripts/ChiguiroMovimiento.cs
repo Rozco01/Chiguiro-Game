adb600fb18e30064d6b3b87dd7d7aef25cbf44ed
Rozco01 <jeffer.rozo.123@gmail.com>
2021-10-30 16:00:33 -0500

    Creacion de animaciones de salto

--- Assets/Scripts/ChiguiroMovimiento.cs
+++ Assets/Scripts/ChiguiroMovimiento.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class ChiguiroMovimiento : MonoBehaviour
 {
 
+    //Salto regulable
+    public bool Saltando;
+    private float Ypos;
+    private int sky_;
+
+
+
+
     public float speed;
     public float maxspeed;
     public float fuerzaSalto;
-    public int saltosMax = 1;
+    public int saltosMax = 2;
+    public LayerMask capaSuelo;
 
 
     private int saltosRestantes;
@@ -25,25 +34,60 @@ public class ChiguiroMovimiento : MonoBehaviour
         rigidbody = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
+        saltosRestantes = saltosMax;
     }
 
     // Update is called once per frame
     void Update()
     {
         ProcesarMovimiento();
-        ProcesarSalto();
+        ProcesarSaltoDerecha();
+        ProcesarSaltoIzquierda();
     }
 
+    bool EstaEnSuelo(){
+        RaycastHit2D raycastHit= Physics2D.BoxCast(boxCollider.bounds.center, new Vector2(boxCollider.bounds.size.x, boxCollider.bounds.size.y), 0f, Vector2.down,0.1f,capaSuelo);
+        return raycastHit.collider != null;
+    }
 
-    void ProcesarSalto(){
-        if(Input.GetKeyDown(KeyCode.Space)){
+        public void ProcesarSaltoIzquierda(){
+        if(EstaEnSuelo()){
+            saltosRestantes = saltosMax;
+            animator.SetBool("sky2", false);
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && saltosRestantes > 0 && (animator.GetBool("IsRunningLeft") == true || animator.GetBool("IsRunningLeft") == false)){
+            animator.SetBool("sky2",true);
+            saltosRestantes--;
             rigidbody.velocity = new Vector2(rigidbody.velocity.x,0f);
             rigidbody.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
-            animator.SetBool("sky",true);
-
-        }else{
-            animator.SetBool("sky",false);
         }
+        if(transform.position.y > Ypos){
+            animator.SetFloat("Gravity",1);
+        }
+        if (transform.position.y < Ypos){
+            animator.SetFloat("Gravity", 0);
+        }
+        Ypos = transform.position.y;
+    }
+
+    public void ProcesarSaltoDerecha(){
+        if(EstaEnSuelo()){
+            saltosRestantes = saltosMax;
+            animator.SetBool("sky", false);
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && saltosRestantes > 0 && (animator.GetBool("IsRunningRight") == true || animator.GetBool("IsRunningRight") == false)){
+            animator.SetBool("sky",true); 
+            saltosRestantes--;
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x,0f);
+            rigidbody.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+        }
+        if(transform.position.y > Ypos){
+            animator.SetFloat("Gravity",1);
+        }
+        if (transform.position.y < Ypos){
+            animator.SetFloat("Gravity", 0);
+        }
+        Ypos = transform.position.y;
     }
 
     void Orientacion(){
